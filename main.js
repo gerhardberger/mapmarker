98f427a501796a720d07f9166919f8a466ddbc25
gellert <gellihegyi@gmail.com>
2016-01-28 12:41:52 +0100

    firefox pls

--- main.js
+++ main.js
@@ -5,26 +5,26 @@ const $ = s => document.getElementById(s)
 const MARKERS = [ 'red', 'blue', 'yellow', 'purple', 'black', 'white', 'green', 'brown' ]
 
 function addMarker (event) {
-  let li = document.createElement('li')
+  var li = document.createElement('li')
   li.classList.add('marker')
 
-  let l = document.createElement('label')
-  l.innerText = 'Location'
+  var l = document.createElement('label')
+  l.innerHTML = 'Location'
   li.appendChild(l)
 
-  let i = document.createElement('input')
+  var i = document.createElement('input')
   i.type = 'text'
   i.classList.add('location')
   li.appendChild(i)
 
-  let s = document.createElement('select')
+  var s = document.createElement('select')
   s.classList.add('color')
 
   MARKERS.map((m, ix) => {
-    let o = document.createElement('option')
+    var o = document.createElement('option')
     if (ix === 0) o.setAttribute('selected', 'true')
     o.value = m
-    o.innerText = m.charAt(0).toUpperCase() + m.slice(1)
+    o.innerHTML = m.charAt(0).toUpperCase() + m.slice(1)
 
     s.appendChild(o)
   })
@@ -37,9 +37,9 @@ function addMarker (event) {
 function draw (center, markers) {
   var src = 'http://maps.googleapis.com/maps/api/staticmap?size=1600x1600&sensor=false'
 
-  let qs = o => {
+  var qs = o => {
     var s = ''
-    for (let k in o) s += `&${k}=${o[k]}`
+    for (var k in o) s += `&${k}=${o[k]}`
     return s
   }
 
@@ -47,21 +47,21 @@ function draw (center, markers) {
 
   src += '&' + markers.map(m => `markers=color:${m.c}|label:|${m.l}`).join('&')
 
-  $('address').innerText = 'Open new window'
+  $('address').innerHTML = 'Open new window'
   $('address').href = src
   $('mapimg').src = src
 }
 
 window.onload = function () {
   document.querySelector('.map-submit').addEventListener('click', event => {
-    let center  = $('mapcenter').value
-    let zoom    = $('mapzoom').value
-    let scale   = $('mapscale').value
-    let maptype = $('maptype').value
+    var center  = $('mapcenter').value
+    var zoom    = $('mapzoom').value
+    var scale   = $('mapscale').value
+    var maptype = $('maptype').value
 
-    let s = []
-    let markers = document.querySelectorAll('.marker')
-    for (let ix = 0; ix < markers.length; ++ix) {
+    var s = []
+    var markers = document.querySelectorAll('.marker')
+    for (var ix = 0; ix < markers.length; ++ix) {
       s.push({
         c: markers[ix].querySelector('.color').value
         , l: markers[ix].querySelector('.location').value
